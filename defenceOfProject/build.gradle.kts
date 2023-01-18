0099b71f592568398bf86330753efb5bd988ad1f
Yaroslav <yb2003work@gmail.com>
2023-01-19 00:43:58 +0300

    build.gradle.kts fixed

--- defenceOfProject/build.gradle.kts
+++ defenceOfProject/build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.flywaydb:flyway-core")
     runtimeOnly("org.postgresql:postgresql")
 }
