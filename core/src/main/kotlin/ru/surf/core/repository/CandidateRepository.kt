f839f708b211f2b8ea4e7acfc3c4d0db29b0da12
KirSanich <aqua_agera_ls4@mail.ru>
2023-02-22 05:16:21 +0300

    TASK-340 Add candidate filtering domain
    Добавление метода поиска кандидатов определенной стажировки, а также метода в контроллере событий

--- core/src/main/kotlin/ru/surf/core/repository/CandidateRepository.kt
+++ core/src/main/kotlin/ru/surf/core/repository/CandidateRepository.kt
@@ -1,7 +1,6 @@
 package ru.surf.core.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import ru.surf.core.entity.Candidate
@@ -10,9 +9,6 @@ import java.util.*
 @Repository
 interface CandidateRepository : JpaRepository<Candidate, UUID> {
 
-    @Query(
-        """SELECT * FROM candidates c WHERE c.event_id =:event_id """, nativeQuery = true
-    )
-    fun getCandidatesByEventId(@Param("event_id") eventID: UUID): List<Candidate>
+    fun getCandidatesByEventId(@Param("event_id") eventId: UUID): List<Candidate>
 
 }
