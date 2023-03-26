86eb69be3fc95c7558654971f77c676166064647
kirsanich <aqua_agera_ls4@mail.ru>
2023-03-26 21:27:36 +0300

    TASK-58 Add Prometheus and ELK support
    Добавление расширенного логирования с поддержкой elastic search, добавление метрик в сервисы core, meeting, mail

--- meeting/src/main/kotlin/ru/surf/meeting/controller/ZoomController.kt
+++ meeting/src/main/kotlin/ru/surf/meeting/controller/ZoomController.kt
@@ -1,18 +1,30 @@
 package ru.surf.meeting.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 import ru.surf.meeting.dto.zoom.ZoomAdminUserResponse
 import ru.surf.meeting.service.ZoomIntegrationService
 import java.util.*
+import java.util.concurrent.ThreadLocalRandom
 
 @RestController
 @RequestMapping("/zoom")
 class ZoomController(private val zoomIntegrationService: ZoomIntegrationService) {
 
+    companion object Logger{
+        val logger = LoggerFactory.getLogger(ZoomController::class.java)
+    }
+
     @GetMapping("/{id}")
     fun getAdminUser(@PathVariable(name = "id") id: String): Mono<ZoomAdminUserResponse> {
         return Mono.just(zoomIntegrationService.getZoomAdminUserInformation(id))
+    }
+
+    @GetMapping("/test")
+    fun get():String{
+        logger.info("Hello ${ThreadLocalRandom.current().nextInt()}")
+        return "test"
     }
 
 }
